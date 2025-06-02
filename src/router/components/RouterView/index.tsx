934cb7ea6b21b319615149d47b7629101462aa73
GaoShuo <1813539137@qq.com>
2025-06-02 12:06:47 +0800

    feat: index

--- src/router/components/RouterView/index.tsx
+++ src/router/components/RouterView/index.tsx
@@ -1,18 +1,19 @@
-import { Suspense } from "react";
+// import { Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Loading from "../Loading";
+// import Loading from "../Loading";
 import routes from "../../routes";
 
 export default function RouterView() {
 	return (
 		<Router>
-			<Suspense fallback={<Loading />}>
-				<Routes>
-					{routes.map((item, index) => (
-						<Route key={index} path={item.path} element={<item.element />} />
-					))}
-				</Routes>
-			</Suspense>
+			{/* 因为加载太快了，所以考虑去掉这个suspense，不然每次都闪一下很难受 */}
+			{/* <Suspense fallback={<Loading />}> */}
+			<Routes>
+				{routes.map((item, index) => (
+					<Route key={index} path={item.path} element={<item.element />} />
+				))}
+			</Routes>
+			{/* </Suspense> */}
 		</Router>
 	);
 }
--- src/views/Index/index.tsx
+++ src/views/Index/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import style from './index.module.css';
 
 const Index: React.FC = () => {
   return (
-    <div>index</div>
+    <div className={style.container}>
+      <div className={style.header}>
+        <div>个人介绍页生成器-PIGG</div>
+      </div>
+    </div>
   )
 }
 
